e2223b228b26df7f2bc09ec666d9b67f4b11c72d
Colston Bod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2024-08-18 10:44:17 +0000

    added main function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello world!")
+import sys
+
+# from module.ClassName import ClassName.solutionMethod
+
+def main():
+  # print(solutionMethod())
+  print("Hello world!")
+
+if __name__ == "__main__":
+  sys.exit(main())
