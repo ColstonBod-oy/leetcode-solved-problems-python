a692a79c9f15a1cf5a4a4d1f7845a3b12451ca4f
Colston Bod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2024-09-07 15:42:24 +0000

    updated surrounded regions

--- graphs/surrounded_regions_130.py
+++ graphs/surrounded_regions_130.py
@@ -10,13 +10,11 @@ class Solution(object):
         dfs(sr, sc + 1)
         dfs(sr, sc - 1)
 
-    for i in range(cols):
-      dfs(0, i)
-      dfs(rows - 1, i)
-
-    for i in range(1, rows - 1):
-      dfs(i, 0)
-      dfs(i, cols - 1)
+    for i in range(rows):
+      for j in range(cols):
+        if (board[i][j] == 'O' and
+            (i in (0, rows - 1) or j in (0, cols - 1))):
+            dfs(i, j)
 
     for i in range(rows):
       for j in range(cols):
