6f8a79cf81001b08b420e5fd18d928cba09ed42e
Colston Bod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2024-09-08 07:57:54 +0000

    updated permutations

--- backtracking/permutations_46.py
+++ backtracking/permutations_46.py
@@ -1,5 +1,3 @@
-import copy
-
 class Solution(object):
   def permute(self, nums):
     res = [[]]
@@ -8,7 +6,7 @@ class Solution(object):
       new_res = []
       for sub in res:
         for i in range(len(sub) + 1):
-          sub_copy = copy.deepcopy(sub)
+          sub_copy = sub[:]
           sub_copy.insert(i, num)
           new_res.append(sub_copy)
       res = new_res
