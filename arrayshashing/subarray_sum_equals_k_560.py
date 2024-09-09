0f43cdfaeb50bed8bff5633c5bc3cfc5e4b8b80b
Colston Bod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2024-09-09 06:55:35 +0000

    updated subarray sum equals k

--- arrayshashing/subarray_sum_equals_k_560.py
+++ arrayshashing/subarray_sum_equals_k_560.py
@@ -1,10 +1,10 @@
 class Solution(object):
   def subarraySum(self, nums, k):
-    myMap = { 0: 1 }
-    curSum, res = 0, 0
+    my_map = { 0: 1 }
+    cur_sum, res = 0, 0
     for n in nums:
-      curSum += n
-      res += myMap.get(curSum - k, 0)
-      myMap[curSum] = 1 + myMap.get(curSum, 0)
+      cur_sum += n
+      res += my_map.get(cur_sum - k, 0)
+      my_map[cur_sum] = 1 + my_map.get(cur_sum, 0)
     return res
       
